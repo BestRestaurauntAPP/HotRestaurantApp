4081a26cdb7c85f5b7dd5c9a6972b55375e80df1
rxi16 <rxi16@users.noreply.github.com>
2017-10-14 12:11:03 -0400

    installed node packages

--- reserve.js
+++ reserve.js
@@ -1,3 +1,10 @@
 $("#add-reservation").on("click", function() {
-
-})
+	var Reservation = {
+		name: $("#name-input").val().trim(),
+		number: $("#number-input").val().trim(),
+		email: $("#email-input").val().trim(),
+		id: $("#id-input").val().trim()
+	};
+	var URL = window.location.origin;
+	$.post
+});
